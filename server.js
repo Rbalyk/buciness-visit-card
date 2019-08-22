ce120010bd2934b3caadecb8e2714d1995221011
roma <rbalik341@gmail.com>
2019-08-22 14:27:53 +0300

    Initial Commit

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/<zoryana-gala>'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/<zoryana-gala>/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
